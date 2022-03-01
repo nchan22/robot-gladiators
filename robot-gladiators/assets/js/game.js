0117bc8eea364ae979f9c4d5e3466a7741425a72
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-03-01 12:38:44 -0500

    Update game.js

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -85,3 +85,9 @@ var fight = function () {
 
 // run fight function to start game
 fight();
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
